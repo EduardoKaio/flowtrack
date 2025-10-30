a456baa2f224f8a4dc78d66d576f662f69dd0129
Erik Medeiros <erik.medeiros.701@ufrn.edu.br>
2025-10-30 11:45:19 -0300

    first commit

---
+++ backend/flowtrack/src/main/java/com/flowtrack/flowtrack/controller/MoodController.java
@@ -0,0 +1,9 @@
+package com.flowtrack.flowtrack.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/mood")
+public class MoodController {
+}
