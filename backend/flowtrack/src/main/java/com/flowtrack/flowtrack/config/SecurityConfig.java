2e61096dada0b529d844566752e03da9fdeb5020
Kaio Alves <kaioeduardooficial@gmail.com>
2025-12-13 20:03:09 -0300

    fix: ajuste no security

--- backend/flowtrack/src/main/java/com/flowtrack/flowtrack/config/SecurityConfig.java
+++ backend/flowtrack/src/main/java/com/flowtrack/flowtrack/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // front-end
+        configuration.setAllowedOrigins(List.of("http://3.131.93.0:3000")); // front-end
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
