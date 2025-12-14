ec61844d5aac835b45f9edad55ab464b377b7ee2
Kaio Alves <kaioeduardooficial@gmail.com>
2025-12-13 23:11:37 -0300

    fix: ajuste migration

--- backend/flowtrack/src/main/resources/db/migration/V3__inserir_dados_categorias.sql
+++ backend/flowtrack/src/main/resources/db/migration/V3__inserir_dados_categorias.sql
@@ -1,5 +1,19 @@
+-- 1. Cria a tabela
+CREATE TABLE IF NOT EXISTS categories (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    color VARCHAR(50),
+    user_id BIGINT,
+    CONSTRAINT fk_categories_user
+        FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- 2. Insere os dados apenas se nao existirem
 INSERT INTO categories (name, color, user_id)
-SELECT v.name, v.color, v.user_id
+SELECT
+    v.name,
+    v.color,
+    CAST(v.user_id AS BIGINT)
 FROM (
     VALUES
         ('Trabalho', 'bg-blue-500', NULL),
@@ -7,13 +21,13 @@ FROM (
         ('Saúde', 'bg-green-500', NULL),
         ('Lazer', 'bg-orange-500', NULL),
         ('Pessoal', 'bg-pink-500', NULL)
-) AS v(name, color, user_id BIGINT)
+) AS v(name, color, user_id)
 WHERE NOT EXISTS (
     SELECT 1
     FROM categories c
     WHERE c.name = v.name
       AND (
             (c.user_id IS NULL AND v.user_id IS NULL)
-            OR c.user_id = v.user_id
+            OR c.user_id = CAST(v.user_id AS BIGINT)
           )
 );
